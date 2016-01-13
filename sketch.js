12dd396cc1098e78649e460e1d3e597c9b0548c5
shelrobin <shelbyrobin7@gmail.com>
2016-01-12 20:45:20 -0500

    hw

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(500,500);
+  background (235,0,0);
+}
+
+function draw() {
+  strokeWeight(0);
+  fill(50,50,50);
+  ellipse (50,50,50,50);
+  
+  strokeWeight(1);
+  fill (255,255,255);
+  ellipse (150,150,50,50);
+}
